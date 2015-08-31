c8475c8c6713b837699ee7486d5f0f19be99d5a0
andlon93 <andreas.lones@gmail.com>
2015-08-31 14:35:28 +0200

    Now handles rectangular boards

--- IT3105(AI_prog)/Module_1/A_star.py
+++ IT3105(AI_prog)/Module_1/A_star.py
@@ -14,7 +14,9 @@ class Node:
     parent = None
     x_pos = None
     y_pos = None
-
+    #-- For A* --#
+    g = None
+    h = None
     def __init__(self):
         self.children = []
 
@@ -30,7 +32,8 @@ def update_board_cell(node, board, letter):
 
 #########---- Take input from user ----########
 def user_input(): #get all input from user
-	board_size = int( raw_input("Breadth and width of board: ") )	#Board size
+	rows = int( raw_input("Height of board: ") )	#Board size
+	cols = int( raw_input("Width of board: ") )	    #Board size
 	#start and end nodes
 	start_node = str( raw_input("position of start node(x,y): ") )
 	goal_node = str( raw_input("position of goal node(x,y): ") )
@@ -45,15 +48,15 @@ def user_input(): #get all input from user
 		print "start_node, goal_node: ", start_node, goal_node
 		print "Barriers: ", Barriers
 		print '\n'
-	return board_size, start_node, goal_node, Barriers
+	return rows, cols, start_node, goal_node, Barriers
 
 #########---- Create board ----########
-def create_empty_board(board_size):#Initialaise an empty board
+def create_empty_board(rows, cols):#Initialaise an empty board
 	board = []
 
-	for row in xrange(0, board_size):
+	for row in xrange(0, rows):
 		column = []
-		for col in xrange(0, board_size):
+		for col in xrange(0, cols):
 			column.append('C')
 		board.append( column )
 
@@ -79,15 +82,15 @@ def add_Barriers(board, Barriers):#Add barreiers to the board
 			print row
 		print '\n'
 	return board	
-def create_board(board_size, start_node, goal_node, Barriers):#Creates board using help functions
-	board = create_empty_board(board_size)#create empty board
+def create_board(rows, cols, start_node, goal_node, Barriers):#Creates board using help functions
+	board = create_empty_board(rows, cols)#create empty board
 	board[ int(start_node[0]) ][ int(start_node[-1]) ] = 'S'#add start node marked 'S'
 	board[ int(goal_node[0]) ][ int(goal_node[-1]) ] = 'G'#add goal node marked 'G'
 	board = add_Barriers(board, Barriers)#add the barriers to the board
 	
 	if debug:
 		print '\n'
-		print board_size
+		print "Rows: ", rows, "columns: ", cols
 		for row in board:
 			print row
 		print '\n'
@@ -171,7 +174,7 @@ def Depth_first_search(board, start_node):
 
 #########---- A* ----########
 def Heuristic():
-	
+
 	pass
 def Astar(board, start_node, end_node):
 
@@ -179,13 +182,14 @@ def Astar(board, start_node, end_node):
 #board_size, start_node, goal_node, Barriers = user_input()
 #create_board(board_size, start_node, goal_node, Barriers)
 #board = create_board(10, '0,0', '9,9', ['2,3,5,5', '8,8,2,1'])
-board = create_board(3, '0,0', '2,2', ['0,1,2,2'])
-rot, goal_node ,class_board = create_linked_classes(board)
+board = create_board(3, 4, '0,0', '2,2', ['0,1,2,2'])
+print_board(board)
+#rot, goal_node ,class_board = create_linked_classes(board)
 
 #print_board(board)
 #print '\n\n'
-BFS_board = Breadth_first_search(board, rot)
-DFS_board = Depth_first_search(board, rot)
+#BFS_board = Breadth_first_search(board, rot)
+#DFS_board = Depth_first_search(board, rot)
 #print_board(board)
 #print '\n\n'
 
