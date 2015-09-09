3f6cc8927bcd851bf18830ca2f1a400975db0c56
andlon93 <andreas.lones@gmail.com>
2015-09-09 11:54:53 +0200

    GUI er opp og gaar

--- IT3105(AI_prog)/Module_1/A_star.py
+++ IT3105(AI_prog)/Module_1/A_star.py
@@ -1,4 +1,7 @@
-import copy
+from threading import Thread, Lock
+import Tkinter as tk
+from Tkinter import *
+import time
 debug = False
 #########---- Letters from the print ----########
 '''
@@ -9,6 +12,7 @@ B = Barrier
 P = path
 O = Open
 '''
+
 #########---- Example sets ----########
 #        row  col start    goal    Barriers
 set_0 = [ 10, 10, [0,0],  [9,9],   [[2,3,5,5], [8,8,2,1]] 										   ]		
@@ -16,7 +20,8 @@ set_1 = [ 20, 20, [19,3], [2,18],  [[5,5,10,10], [1,2,4,1]] 									   ]
 set_2 = [ 20, 20, [0,0],  [19,19], [[17,10,2,1], [14,4,5,2], [3,16,10,2], [13,7,5,3], [15,15,3,3]] ]
 set_3 = [ 10, 10, [0,0],  [9,5],   [[3,0,2,7], [6,0,4,4], [6, 6, 2, 4]] 						   ]
 set_4 = [ 10, 10, [0,0],  [9,9],   [[3,0,2,7], [6,0,4,4], [6, 6, 2, 4]] 						   ]
-set_5 = [ 20, 20, [0,0],  [19,13], [[4,0,4,16], [12,4,2,16], [16,8,4,4]] 						   ]
+set_5 = [ 20, 20, [0,0],  [19,13], [[4,0,4,16], [12,4,2,16], [16,8,4,4]]						   ]
+set_6 = [ 100, 100, [0,0],  [99,99], [[4,4,40,50], [45,45,54,40], [44,4,45,3], [89,4,2,30], [4,44,3,50],[90, 50, 9, 50]] 						   ]
 
 #########---- Node Class ----########
 class Node:
@@ -52,7 +57,7 @@ def print_board(board):#prints board with 0,0 in bottom left corner  #Also gener
 				htmlString = htmlString + '<td class="P">' + board[row-1][i] + "</td>"
 			else:
 				htmlString = htmlString + '<td>' + board[row-1][i] + "</td>"
-		print board[row-1], '\n'
+		#print board[row-1], '\n'
 		htmlString = htmlString + "</tr>"
 	htmlString = htmlString + "</table></body>"
 	f = open('output.html','w')
@@ -72,6 +77,19 @@ def update_board_with_path(board, rot, goal_node, path):
 	board[goal_node.x_pos][goal_node.y_pos] = 'G'
 	return board
 
+def draw_GUI(board):
+	reverse_board = []
+	for n in xrange( len(board)-1, -1, -1):
+			reverse_board.append(board[n])
+	for n in xrange(0, len(reverse_board)):
+		for i in xrange(0, len(reverse_board[n])):
+			#Label(text=reverse_board[n][i], width=5).grid(row=n, column=i)
+			if reverse_board[n][i] == '-': Entry( bg="lavender", width=4 ).grid(row=n, column=i)
+			elif reverse_board[n][i] == 'O': Entry(bg="blue", width=4).grid_configure(row=n, column=i)
+			elif reverse_board[n][i] == 'B': Entry( bg="black", width=4 ).grid(row=n, column=i)
+			elif reverse_board[n][i] == 'S': Entry( bg="yellow", width=4 ).grid(row=n, column=i)
+			elif reverse_board[n][i] == 'P': Entry( bg="sky blue", width=4 ).grid(row=n, column=i)
+			else: Entry( bg="green", width=4 ).grid(row=n, column=i)
 #########---- Take input from user ----########
 def user_input(): #get all input from user
 	rows = int( raw_input("Height of board: ") )	#Board size
@@ -91,7 +109,6 @@ def user_input(): #get all input from user
 		print "Barriers: ", Barriers
 		print '\n'
 	return rows, cols, start_node, goal_node, Barriers
-
 #########---- Create board ----########
 def create_empty_board(rows, cols):#Initialaise an empty board
 	board = []
@@ -142,8 +159,6 @@ def create_board(rows, cols, start_node, goal_node, Barriers):#Creates board usi
 	#print_board(board)
 	
 	return board
-#b = create_board( set_0[0], set_0[1], set_0[2], set_0[3], set_0[4] )
-#print_board(b)
 #########---- Create the nodes ----########
 def create_linked_classes(board):
 	class_board = []#matrix with each node as an object
@@ -197,25 +212,26 @@ def create_linked_classes(board):
 		print "parent: ", rot.children[0].parent, '\n'
 		print "goal: ", goal.typ
 	return rot, goal, class_board
-#
 #########---- BFS ----########
 def Breadth_first_search(board, start_node, goal_node):
 	print "BFS start"
 	queue = [[start_node]]
 	visited = set()
+	iterations = []
 	#
 	while queue:
 		path = queue.pop(0)
 
 		current_node = path[-1]
-		print "current_node:", current_node.x_pos, current_node.y_pos
-		board[current_node.x_pos][current_node.y_pos] = 'O'
+		#print "current_node:", current_node.x_pos, current_node.y_pos
+		#board[current_node.x_pos][current_node.y_pos] = 'O'
 		#
 		if current_node == goal_node:
 			#print "GGGGOOOOAAAAALLLL"
-
-			return board, path
+			return path, iterations
 		elif current_node not in visited:
+			if current_node != rot and current_node != goal_node:
+				iterations.append( [current_node.x_pos, current_node.y_pos, 'O'] )
 			for child in current_node.children:
 				temp_path = list(path)
 				#print 'temp_path: ', temp_path
@@ -225,7 +241,8 @@ def Breadth_first_search(board, start_node, goal_node):
 				#print 'queue: ', queue
 				#
 			visited.add(current_node)
-			print len(visited)
+		#time.sleep(0.1)
+			#print len(visited)
 			#print '------------New iteration---------------'
 #
 #########---- DFS ----########			
@@ -278,7 +295,6 @@ def bubble_sort(items):
         for j in range(len(items)-1-i):
             if items[j].f > items[j+1].f:
                 items[j], items[j+1] = items[j+1], items[j]
-
 #
 def Astar(board, start_node, end_node):
 	closed=[]
@@ -305,16 +321,20 @@ def Astar(board, start_node, end_node):
 				attach_and_eval(child,current_node)
 				open.append(child)
 				board[child.x_pos][child.y_pos]="O"
+				
+
+				##################################
+
+				#####################################
 				bubble_sort(open)
 			elif ((current_node.g + 1) < child.g):
 
-				print "FOUND CHEAPER PATH"
+				#print "FOUND CHEAPER PATH"
 				#print '------4------'
 				attach_and_eval(child,current_node)
 				if child in closed:
 					prop_path_imp(child)
-	
-
+#
 def findPath(start_node, goal_node):
 	coordinates = []
 	current_node = goal_node
@@ -322,7 +342,7 @@ def findPath(start_node, goal_node):
 
 	while True:
 		if current_node == start_node:
-			print coordinates
+			#print coordinates
 			board[start_node.x_pos][start_node.y_pos]="S"
 			return True
 		else:
@@ -330,45 +350,133 @@ def findPath(start_node, goal_node):
 			coordinates.append(coords)
 			current_node = current_node.parent
 			board[current_node.x_pos][current_node.y_pos] = "P"
-
-
-
 #
-#			
 #board_size, start_node, goal_node, Barriers = user_input()
-#create_board(board_size, start_node, goal_node, Barriers)
-#board = create_board(10, '0,0', '9,9', ['2,3,5,5', '8,8,2,1'])
-#board = create_board(3, 4, [0,0], [2,2], [[0,1,2,2]])
-
 #board = create_board( set_0[0], set_0[1], set_0[2], set_0[3], set_0[4] )
-board = create_board( set_5[0], set_5[1], set_5[2], set_5[3], set_5[4] )
+#board = create_board( set_6[0], set_6[1], set_6[2], set_6[3], set_6[4] )
+###################################################################
+def draw(reverse_board, iterations):
+	reverse_board [ len(reverse_board)-iterations[0][0]-1 ] [ iterations[0][1] ] = iterations[0][2]
+	iterations.pop(0)
+	for n in xrange(0, len(reverse_board)):
+		for i in xrange(0, len(reverse_board[n])):
+			#Label(text=reverse_board[n][i], width=5).grid(row=n, column=i)
+			if reverse_board[n][i] == '-': Entry( bg="lavender", width=4 ).grid(row=n, column=i)
+			elif reverse_board[n][i] == 'O': Entry( bg="blue", width=4).grid_configure(row=n, column=i)
+			elif reverse_board[n][i] == 'B': Entry( bg="black", width=4 ).grid(row=n, column=i)
+			elif reverse_board[n][i] == 'S': Entry( bg="yellow", width=4 ).grid(row=n, column=i)
+			elif reverse_board[n][i] == 'P': Entry( bg="sky blue", width=4 ).grid(row=n, column=i)
+			else: Entry( bg="green", width=4 ).grid(row=n, column=i)
+	root.after( 200, draw, reverse_board, iterations )
 
-#print_board(board)
-rot, goal_node, class_board = create_linked_classes(board)
 
-####boool = Astar(board, rot, goal_node)
-####print boool
+#def GUI(board, iterations):
+	
+
+	
+
+	#print iterations[0][2]
+	#print reverse_board [ len(reverse_board)-iterations[0][0]-1 ] [ iterations[0][1] ]
+	#draw(reverse_board)
 
 
+
+
+	#root.mainloop()
+###################################################################
+
+
+
+
+'''print "Astar start"
 boool = Astar(board, rot, goal_node)
+print "Astar done"
 print_board(board)
-
+print "All done"'''
 #print_board(board)
 #print '\n\n'
-
-#BFS_board = Breadth_first_search(board, rot)
+#
 #DFS_board = Depth_first_search(board, rot)
-#Astar_board = Astar(board,rot,goal_node)
-#print_board(Astar_board)
+#
+def update_iterations_with_path(iterations, path, board):
+	for cell in path:
+		#print "cell", cell
+		if board[cell.x_pos][cell.y_pos] != 'S' and board[cell.x_pos][cell.y_pos] != 'G': iterations.append( [cell.x_pos, cell.y_pos, 'P'] )
+	return iterations
 
+
+board = create_board( set_5[0], set_5[1], set_5[2], set_5[3], set_5[4] )
+rot, goal_node, class_board = create_linked_classes(board)
+path, iterations = Breadth_first_search(board, rot, goal_node)
+
+print "path: ", path[0]
+iterations = update_iterations_with_path(iterations, path, board)
+
+
+root = Tk()
+reverse_board = []
+for n in xrange( len(board)-1, -1, -1):
+	reverse_board.append(board[n])
+	
+	
+
+root.after( 200, draw, reverse_board, iterations )
+root.mainloop()
+#GUI(board, iterations)
+#print "path: ", path
+#board = update_board_with_path(board, rot, goal_node, path)
+#print "BFS: \n"
+#print_board(board)
 '''
-board, path = Breadth_first_search(board, rot, goal_node)
-print "path: ", path
-board = update_board_with_path(board, rot, goal_node, path)
-print "BFS: \n"
-print_board(board)
-
 #
 #print "\nDFS: \n"
 #DFS_board = Depth_first_search(board, rot)
 #print_board(board)'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''Height = Label(frame1, text="Height", fg="black", font=("times", 15))#.grid(row=0, column=0)#.place(x=x, y=y)
+Width = Label(frame1, text="Width", fg="black", font=("times", 15))#.grid(row=0, column=1)#.place(x=x+70, y=y+0)
+start = Label(frame1, text="start point", fg="black", font=("times", 15))#.grid(row=0, column=2)#.place(x=x+140, y=y+0)
+goal = Label(frame1, text="Goal point", fg="black", font=("times", 15))#.grid(row=0, column=3)#.place(x=x+240, y=y+0)
+barriers = Label(frame1, text="Barriers", fg="black", font=("times", 15))#.grid(row=0, column=4)#.place(x=x+360, y=y+0)
+
+height_entry = Entry(frame1, width=3)
+width_entry = Entry(frame1, width=3)
+start_entry = Entry(frame1, width=4)
+goal_entry = Entry(frame1, width=4)
+barriers_entry = Entry(frame1, width=40)
+
+draw_board_button = Button(frame1, text="draw board", bg="sky blue", command=draw_GUI(board))
+
+Height.pack()#grid(row=0, column=0)
+height_entry.pack()#grid(row=0, column=1)
+Width.pack()#grid(row=0, column=2)
+width_entry.pack()#grid(row=0, column=3)
+start.pack()#grid(row=0, column=4)
+start_entry.pack()#grid(row=0, column=5)
+goal.pack()#grid(row=0, column=6)
+goal_entry.pack()#grid(row=0, column=7)
+barriers.pack()#grid(row=0, column=8)
+barriers_entry.pack()#grid(row=0, column=9)
+draw_board_button.pack()#grid(row=0, column=10)
+frame1.grid(row=0, rowspan=10, column=211, columnspan=10, sticky = W+E+N+S)'''
