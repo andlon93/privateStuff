e438a955034f3a3a28076b9dbe09f851acb78306
andlon93 <andreas.lones@gmail.com>
2015-09-18 11:28:14 +0200

    reading in file

--- IT3105(AI_prog)/Module_2/State.py
+++ IT3105(AI_prog)/Module_2/State.py
@@ -1,24 +1,18 @@
 class State:
 	nodes = {} #id: peker til objekt
 	heuristic = None #heuristic til denne staten
-
+	#
 	def __init__(self, nodes):
 		self.nodes = nodes
 		heuristic = self.calculate_heuristic(nodes)
-
+	#
 	def calculate_heuristic(self, nodes):
 		h = 0
-		for i in nodes:
-			h = h + len(nodes[i]) - 1
-			print h
+		for node in nodes:
+			h = h + len(nodes[node].domain) - 1
 		return h
-
+	#
 	def revise(node, c):
 		pass
-
-nodes = {0: [0, 1, 2],
-         1: [0, 1, 2],
-	     2: [0]
-				     }
-
-s = State(nodes)
+#
+#s = State(nodes)
--- IT3105(AI_prog)/Module_2/readfile.py
+++ IT3105(AI_prog)/Module_2/readfile.py
@@ -1,30 +1,33 @@
 # Read from file
-
 # vertices edges
-
-
-def createNode(index, x_pos, y_pos):
-	print 'createnode: ' + index,x_pos,y_pos
-
+print "GO"
+import Node
+import State
+#
 def read_graph(path):
+	domain_size = 4
+	#
 	f = open(path, 'r')
-
-	vertices_and_edges =f.readline().split()
-	vertices = int(vertices_and_edges[0])
-	edges = int(vertices_and_edges[1])
-
-	i=0
+	#
+	vertices_and_edges =f.readline().split()#read number of vertices and edges
+	vertices = int(vertices_and_edges[0])#number of certices
+	edges = int(vertices_and_edges[1])#number of edges
+	#
+	i=0 #iterate through all vertices and create Node objects of them
+	Node_dict = {}
 	while i < (vertices):
 		temp = f.readline().split()
-		createNode(temp[0],temp[1],temp[2])
-		i    = i + 1
-
+		node = Node.Node(temp[0],temp[1],temp[2], domain_size)
+		Node_dict[node.index] = node
+		i = i + 1
+	#Iterate through the constraints
 	constraints = []
 	k=0
-	while k<edges:
+	while k < edges:
 		temp2 = f.readline().split()
-		constraints.append((temp2[0],temp2[1]))
-		k     = k + 1
-
-
-
+		constraints.append( (int(temp2[0]), int(temp2[1])) )
+		k = k + 1
+	#
+	Start_State = State.State(Node_dict)
+	return Start_State, constraints
+#read_graph("graph1.txt")
