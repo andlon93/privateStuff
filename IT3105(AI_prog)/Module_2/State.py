23cb8952e6c81bd306963f91015132ab65a6fba9
andlon93 <andreas.lones@gmail.com>
2015-09-18 10:56:54 +0200

    ret

--- IT3105(AI_prog)/Module_2/State.py
+++ IT3105(AI_prog)/Module_2/State.py
@@ -2,12 +2,23 @@ class State:
 	nodes = {} #id: peker til objekt
 	heuristic = None #heuristic til denne staten
 
-	def __init__(nodes):
+	def __init__(self, nodes):
 		self.nodes = nodes
-		heuristic = calculate_heuristic()
+		heuristic = self.calculate_heuristic(nodes)
 
-	def calculate_heuristic():
-		return 1
+	def calculate_heuristic(self, nodes):
+		h = 0
+		for i in nodes:
+			h = h + len(nodes[i]) - 1
+			print h
+		return h
 
 	def revise(node, c):
 		pass
+
+nodes = {0: [0, 1, 2],
+         1: [0, 1, 2],
+	     2: [0]
+				     }
+
+s = State(nodes)
