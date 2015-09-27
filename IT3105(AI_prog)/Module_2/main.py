dfdd4d07bf21d5fe94daa3e58183a262d5877726
olehalvor <olli408@hotmail.com>
2015-09-27 22:30:12 +0200

    Everything is run from main, Seperated GUI to gui.py

--- IT3105(AI_prog)/Module_2/main.py
+++ IT3105(AI_prog)/Module_2/main.py
@@ -1,4 +1,12 @@
 import subprocess
+from threading import *
+import time
+import Astar_GAC
+
+
+def changeDelay():
+	time.sleep(2)
+	Astar_GAC.algorithm_delay = delay
 
 print ""
 print "-------------------------------"
@@ -6,18 +14,22 @@ print "Use GUI? Yes: '1', No: '0'"
 use_gui = int( raw_input("") )
 
 if use_gui==1:
-	print "Input algorithm delay in seconds, blank to use default(0)"
+	print "Input algorithm delay in seconds, blank to use default (0)"
 	try:
 		delay = float ( raw_input(""))
 	except:
 		delay=0
+	t = Thread(target=changeDelay)
+	t.start()
 	import gui
 	raw_input("Press any key to stop python processes?")
 	# import Astar_GAC
 else:
 	print "Which graph do you want to solve?"
 	graph = int ( raw_input(""))
-	import Astar_GAC
 	Astar_GAC.run(graph)
 	raw_input("Quit?")
+
+
+
 subprocess.call("taskkill /F /IM python.exe", shell=True)
