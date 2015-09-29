52fcad133133d29b9cb67bd850b71d799eb5e082
andlon93 <andreas.lones@gmail.com>
2015-09-29 14:37:34 +0200

    rr

--- IT3105(AI_prog)/Module_3/Variable.py
+++ IT3105(AI_prog)/Module_3/Variable.py
@@ -1,3 +1,4 @@
+import itertools
 class Variable:
 	domain = []
 	is_row = None
@@ -9,27 +10,13 @@ class Variable:
 		self.create_full_domain(blocks, length)
 	#
 	def create_full_domain(self, blocks, length):#create the full domain
-		n = length
-		for x in range(2**n):
-  			string =  ''.join(str((x>>i)&1) for i in xrange(n-1,-1,-1))
-  			for block in blocks:
-  				
-
-
-		'''max_start_index = length + 1
-		for b in blocks:
-			max_start_index = max_start_index - b - 1
-		print "max_start_index", max_start_index
-
-		for start_index in xrange(max_start_index+1):
-			while len(templist) < length+1:
-				for block in blocks:
-					for additions in xrange(block):
-						templist.append(True)
-					templist.append(False)
+		liste = []
+		for block in blocks:
+			liste.append(True)
+		while len(liste) < length:
+			liste.append(False)
 		
-
-		self.domain = [0]'''
+		pass
 	##-- Getters --##
 	def get_domain(self): return self.domain
 	def get_is_row(self): return self.is_row
