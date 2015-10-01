5874bfcaf3f8b766efa8191e1ef2f713e2e9362c
andlon93 <andreas.lones@gmail.com>
2015-10-01 12:00:14 +0200

    xrange

--- IT3105(AI_prog)/Module_3/readfile.py
+++ IT3105(AI_prog)/Module_3/readfile.py
@@ -14,17 +14,17 @@ def read_graph(path):
 	cols_and_rows_size =f.readline().split()
 	cols_size = int(cols_and_rows_size[0])
 	rows_size = int(cols_and_rows_size[1])
-	for i in range (rows_size):
+	for i in xrange (rows_size):
 		rows.append((f.readline().split()))
-	for i in range (cols_size):
+	for i in xrange (cols_size):
 		cols.append((f.readline().split()))
 
 	variable_rows = []
 	variable_cols = []
-	for i in range (cols_size):
-		variable_cols.append( Variable.Variable(False, i, cols[i],rows_size) )
-	for i in range (rows_size):
+	for i in xrange (rows_size):
 		variable_rows.append( Variable.Variable(True, i, rows[i], cols_size) )
+	for i in xrange (cols_size):
+		variable_cols.append( Variable.Variable(False, i, cols[i],rows_size) )
 	Start_state = State.State(variable_rows, variable_cols, None)
 	return Start_state
 
