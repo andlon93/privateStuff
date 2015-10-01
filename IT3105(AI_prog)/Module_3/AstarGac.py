71071a15f1386f74838dafbd315d93204321dd3d
andlon93 <andreas.lones@gmail.com>
2015-10-01 12:33:19 +0200

    hh

--- IT3105(AI_prog)/Module_3/AstarGac.py
+++ IT3105(AI_prog)/Module_3/AstarGac.py
@@ -59,72 +59,73 @@ def possible_to_update(t):#Checks whether any of the cells van be updated
 def update_cell(C):#updates the cell in Board if possible
 	d=C[1].get_domain()
 	temp_list=[None]*len(d[0])
+	if len(d) == 1:
+		
 	#
 	for n in xrange(len(d[0])):#sets temp_list to the first list in domain
 		temp_list[n]=int(d[0][n])
-	print "temp_list:  ", temp_list
+	#print "temp_list:  ", temp_list
 	##--if a cell can have two values, it cannot be set to a value--##
 	for n in xrange(1, len(d)):
 		for i in xrange(len(d[n])):
-			print "I:",i
-			print "hvis temp_list[i]  !=  ", int(d[n][i]), "then temp_list[i]=-1"
+			#print "I:",i
+			#print "hvis temp_list[i]  !=  ", int(d[n][i]), "then temp_list[i]=-1"
 			if temp_list[i]!=int(d[n][i]): 
 				temp_list[i] = -1
-		print temp_list
+		#print temp_list
 		if not possible_to_update(temp_list): return [] #temp_list should be all -1's
 	return temp_list
 #
 def update_state_cell_rows(C, state, new_list):#updates state board rows
 	#
-	print "board for oppdatering"
-	for s in state.get_board():
-		print s
+	#print "board for oppdatering"
+	#for s in state.get_board():
+	#	print s
 	#
 	for n in xrange(len(new_list)):
 		if state.board[C[1].get_index()][n]==-1:
 			state.board[C[1].get_index()][n]=int(new_list[n])
 	#
-	print "board etter oppdatering"
-	for s in state.get_board():
-		print s
+	#print "board etter oppdatering"
+	#for s in state.get_board():
+	#	print s
 def update_state_cell_cols(C, state, new_list):#updates state board columns
 	#
-	print "board for oppdatering"
-	for s in state.get_board():
-		print s
+	#print "board for oppdatering"
+	#for s in state.get_board():
+	#	print s
 	#
 	for n in xrange(len(new_list)):
 		if state.board[n][C[1].get_index()]==-1:
 			state.board[n][C[1].get_index()]=int(new_list[n])
 	#
-	print "board etter oppdatering"
-	for s in state.get_board():
-		print s
+	#print "board etter oppdatering"
+	#for s in state.get_board():
+	#	print s
 #
 def update_variable_domain(C):#updates the domain based on a cell value
 	index = C[0][0]
 	d=C[1].get_domain()
+	new_domain = []
 	#
-	print "domain:"
+	#print "domain:"
 	for n in d:
-		print n
-	for n in d:
-		print "Hvis  ", n[index], "  !=  ", str(C[0][1]), "fjern fra domain"
-		if n[index] != str(C[0][1]):
-			d.remove(n)
-			print "de var ikke like: ny len", len(d)
-	print '\n'
-	return d
+		#print "Hvis  ", n[index], "  ==  ", str(C[0][1]), "legg til i new domain"
+		if n[index] == str(C[0][1]):
+			new_domain.append(n)
+			#print "de var like: ny len", len(new_domain)
+	#print '\n'
+	return new_domain
 #
 def revice(state, C):#changes a state based on a constraint
 	domain_updated=False
 	cells_updated=False
-	if C[0][1]==-1:
-		print "UPDATING CELLS"
+	if C[0][1] == -1:
+		#print "UPDATING CELLS"
 		new_list=update_cell(C)
-		print "new_list:  ", new_list
+		#print "new_list:  ", new_list
 		##--update cell rows or columns if needed--##
-		if len(new_list)>0:
+		if len(new_list)>0 and len(C[1].get_domain()) > 1:
 			if C[1].get_is_row():
 				update_state_cell_rows(C, state, new_list)
 			else:
@@ -132,13 +133,13 @@ def revice(state, C):#changes a state based on a constraint
 			cells_updated=True
 		##
 	else:
-		print "UPDATING DOMAIN"
+		#print "UPDATING DOMAIN"
 		old_domain_len=len(C[1].get_domain())
 		new_domain=update_variable_domain(C)
 
 		##--update domain if needed--##
 		if old_domain_len>len(new_domain):
-			print "new domain", new_domain
+			#print "new domain", new_domain
 			if C[1].get_is_row():
 
 				state.set_row(C[1].get_index(), new_domain)
@@ -155,11 +156,11 @@ def extend_queue_domain(C, state):#extends the GAC_queue when needed
 	new_c = deque()
 	if C[1].get_is_row():
 		for index in xrange(len(C[1].get_domain()[0])):
-			print "new constraint:  ", [ [index, state.get_board_cell(C[1].get_index(), index)], C[1] ]
+			#print "new constraint:  ", [ [index, state.get_board_cell(C[1].get_index(), index)], C[1] ]
 			new_c.append( [ [index, state.get_board_cell(C[1].get_index(), index)], C[1] ] )
 	else:
 		for index in xrange(len(C[1].get_domain()[0])):
-			print "new constraint  ", [[index, state.get_board_cell( index, C[1].get_index() )], C[1]]
+			#print "new constraint  ", [[index, state.get_board_cell( index, C[1].get_index() )], C[1]]
 			new_c.append( [ [index, state.get_board_cell( index, C[1].get_index() )], C[1] ] )
 	return new_c
 #
@@ -167,32 +168,33 @@ def extend_queue_cells(C, state):
 	new_c = deque()
 	if C[1].get_is_row():
 		for index in xrange(len(C[1].get_domain()[0])):
-			print "Hvis ikke  ", state.get_board_cell(C[1].get_index(), index), " == -1" 
+			#print "Hvis ikke  ", state.get_board_cell(C[1].get_index(), index), " == -1" 
 			if state.get_board_cell(C[1].get_index(), index) != -1:
-				print "new constraint:  ", [ [index, state.get_board_cell(C[1].get_index(), index)], C[1] ]
+				#print "new constraint:  ", [ [index, state.get_board_cell(C[1].get_index(), index)], C[1] ]
 				new_c.append( [ [index, state.get_board_cell(C[1].get_index(), index)], C[1] ] )
 	else:
 		for index in xrange(len(C[1].get_domain()[0])):
-			print "Hvis ikke  ", state.get_board_cell(C[1].get_index(), index), " == -1"
-			if state.get_board_cell(C[1].get_index(), index) != -1:
-				print "new constraint:  ", [ [index, state.get_board_cell(index, C[1].get_index())], C[1] ]
+			#print "Hvis ikke  ", state.get_board_cell(index, C[1].get_index()), " == -1"
+			if state.get_board_cell(index, C[1].get_index()) != -1:
+				#print "new constraint:  ", [ [index, state.get_board_cell(index, C[1].get_index())], C[1] ]
 				new_c.append( [ [index, state.get_board_cell(index, C[1].get_index())], C[1] ] )
 	return new_c
 #
 def Filter(state, queue):#Iterates through the GAC_queue -> runs revice on them
-	#while queue:
-	for Foo in xrange(1):
+	while queue:
 		C = queue.popleft()
-		print "NY CONSTRAINT:  ", C[0][0], C[0][1], C[1].get_domain()
+		#print "NY CONSTRAINT:  ", C[0][0], C[0][1], C[1].get_domain()
 		domain_updated, cells_updated = revice(state, C)
 		if domain_updated:
-			print "domain updated"
+			#print "domain updated"
 			queue.extend(extend_queue_domain(C, state))
-			print "\n"
+			#print "\n"
 		if cells_updated:
-			print "cells updated"
+			#print "cells updated"
 			queue.extend(extend_queue_cells(C, state))
-			print "\n"
+			#print "\n"
+		
+	
 #
 ###--- Methods to check validity of a state ---###
 def is_valid_state(state):
@@ -212,21 +214,23 @@ def Astar(start_state):
 	#
 	children = generate_child_states(start_state)
 	for c in children:
-		print "assumption: ", c.get_assumption()
+		#print "assumption: ", c.get_assumption()
 		queue = create_GAC_queue(c, c.get_assumption())
-		print "Queue:"
-		for q in queue:
-			print q[0], q[1].get_is_row(), q[1].get_index()
+		#print "Queue:"
+		#for q in queue:
+		#	print q[0], q[1].get_is_row(), q[1].get_index()
 		#for q in queue:
 		#	print q[1].get_is_row()
 		#print "\n"
 		print "h for Filter", c.get_h()
-		Filter(c, queue)
-		c.set_h(c.calculate_h())
-		print "h etter Filter", c.get_h(), "  g verdi", c.get_g(), '\n\n'
 		for row in c.get_board():
 			print row
-		print '\n\n\n'
+		Filter(c, queue)
+		c.set_h(c.calculate_h())
+		print "h etter Filter", c.get_h(), "  g verdi", c.get_g(),"\n"
+		for row in c.get_board():
+			print row
+		print '\n\n'
 
 if __name__ == '__main__':
 	Astar(rf.read_graph("nono-cat.txt"))
