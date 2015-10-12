bbdd0f7ad0fb52c0da440ebfdbe14504d20e311c
andlon93 <andreas.lones@gmail.com>
2015-10-12 11:04:51 +0200

    move down med merge funker

--- IT3105(AI_prog)/Module_4/State.py
+++ IT3105(AI_prog)/Module_4/State.py
@@ -25,6 +25,18 @@ class State:
 					rute_ledig.popleft()
 					rute_ledig.append([row, col])
 #
+	def move_down(self, col):
+		'''For forklaring se move_up()'''
+		rute_ledig = deque()
+		for row in xrange(3, -1, -1):
+			if self.board[row][col] == 0: rute_ledig.append([row, col])
+			else:
+				if rute_ledig:
+					self.board[rute_ledig[0][0]][rute_ledig[0][1]] = self.board[row][col]
+					self.board[row][col] = 0
+					rute_ledig.popleft()
+					rute_ledig.append([row, col])
+#
 	def move(self, direction):
 		'''updates the board based on a move in a given direction
 		   May add submethods for moving the tiles and merging tiles
@@ -32,19 +44,30 @@ class State:
 		   1 = right
 		   2 = down
 		   3 = left
-		'''
-		
+		'''	
 		if direction == 0:#hvis move er oppover
 			for col in xrange(4):#iterer over brett kolonne for kolonne
 				self.move_up(col)#Move tiles up
 				##-- Merge start --##
 				rute_ledig = deque()
 				for row in xrange(3):
-					if self.board[row][col] == self.board[row+1][col]:
-						board[row][col] = board[row][col] * 2
-						self.board[row+1][col] = 0
+					if self.board[row][col] == self.board[row+1][col]:#merge ruter om de er like
+						board[row][col] = board[row][col] * 2#sett overste til dobbel verdi
+						self.board[row+1][col] = 0#sett den under til 0
 				##-- Merge end --##
 				self.move_up(col)#move tiles up again
+		elif direction == 2:
+			for col in xrange(3, -1, -1):
+				self.move_down(col)
+				##-- Merge start --##
+				rute_ledig = deque()
+				for row in xrange(3, 0, -1):
+					if self.board[row][col] == self.board[row-1][col]:#merge ruter om de er like
+						board[row][col] = board[row][col] * 2#sett overste til dobbel verdi
+						self.board[row-1][col] = 0#sett den under til 0
+				##-- Merge end --##
+				self.move_down(col)
+
 
 
 		pass
@@ -82,14 +105,14 @@ class State:
 	def get_h(self): return self.h
 #
 if __name__ == '__main__':
-	board =[[0,2,0,4],
-			[0,2,2,0],
+	board =[[4,4,0,4],
+			[0,4,2,0],
 			[0,2,0,0],
-			[0,2,2,2]]
+			[4,2,2,2]]
 	s = State(board)
 	for row in s.get_board():
 		print row
 	print '\n'
-	s.move(0)
+	s.move(2)
 	for row in s.get_board():
 		print row
