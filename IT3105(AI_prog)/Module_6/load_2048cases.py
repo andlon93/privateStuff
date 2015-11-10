7bc1e4e88a3ed28185fbe076aeaf2e7aedc604f7
andlon93 <andreas.lones@gmail.com>
2015-11-10 14:54:08 +0100

    working

--- IT3105(AI_prog)/Module_6/load_2048cases.py
+++ IT3105(AI_prog)/Module_6/load_2048cases.py
@@ -1,12 +1,13 @@
+import numpy as np
 def readfile(filename):
 	f = open(filename, 'r')
 	states = []
 	moves = []
 	for line in f.readlines():
 		arr = line.split(',')
-		states.append([float(i) for i in arr[:16]])
-		moves.append([float(i) for i in arr[16:]])
-	return(states,moves)
+		states.append([float(i)/2048 for i in arr[:16]])
+		moves.append([int(i) for i in arr[16:]])
+	return(np.array(states),np.array(moves))
 
 if __name__ == '__main__':
 	b,m = readfile("2048training.txt")
