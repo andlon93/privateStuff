49469afbc17ed2aa8792aaf5df0f601b32b99218
olehalvor <olli408@hotmail.com>
2015-11-10 14:08:49 +0100

    cleanup

--- IT3105(AI_prog)/Module_6/load_2048cases.py
+++ IT3105(AI_prog)/Module_6/load_2048cases.py
@@ -4,10 +4,10 @@ def readfile(filename):
 	moves = []
 	for line in f.readlines():
 		arr = line.split(',')
-		states.append(map(int, arr[0:16]))
+		states.append(map(int, arr[:16]))
 		moves.append(map(int, arr[16:]))
 	return(states,moves)
 
 if __name__ == '__main__':
 	b,m = readfile("2048training.txt")
-	print (m)
+	print (b)
