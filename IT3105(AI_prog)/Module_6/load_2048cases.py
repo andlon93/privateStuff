3d3f43eaf3b223fd41ee84689ca61ad17686ae31
olehalvor <olli408@hotmail.com>
2015-11-10 14:53:15 +0100

    flatten vector input

--- IT3105(AI_prog)/Module_6/load_2048cases.py
+++ IT3105(AI_prog)/Module_6/load_2048cases.py
@@ -1,3 +1,4 @@
+import numpy as np
 def readfile(filename):
 	f = open(filename, 'r')
 	states = []
@@ -5,9 +6,14 @@ def readfile(filename):
 	for line in f.readlines():
 		arr = line.split(',')
 		states.append([float(i) for i in arr[:16]])
-		moves.append([float(i) for i in arr[16:]])
-	return(states,moves)
+		moves.append([int(i) for i in arr[16:]])
+	labels = np.array(moves)
+	labels = labels.flatten()
+	label_vectors = np.zeros((len(labels),4))#init all vectors to zero
+	label_vectors[np.arange(len(labels)),labels] = 1#the right answer is 1
+	return(states,label_vectors)
 
 if __name__ == '__main__':
+	print("GO")
 	b,m = readfile("2048training.txt")
-	print (b[0])
+	print (m[1])
